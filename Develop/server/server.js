e945de73455c3a134ce977a5d1158af4108c6d7a
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-08 17:00:51 -0400

    added routes to server file for html

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 let app = express();
 const PORT = 3001;
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
