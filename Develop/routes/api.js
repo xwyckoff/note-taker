228c7dd261a4772b28a8a7828a6ebd499cfd6b4b
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-04-08 22:31:02 -0400

    added routes for api

---
+++ Develop/routes/api.js
@@ -0,0 +1,12 @@
+const app = require("express").Router();
+const fs = require('fs');
+
+app.get('/notes', (req, res) => {
+
+});
+
+app.post('/notes', (req, res) => {
+
+});
+
+module.exports = app;
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const api = require('../routes/api');
 
 let app = express();
 const PORT = 3001;
+
+app.use('/api', api);
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
